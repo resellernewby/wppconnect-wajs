4dd23c66c8b8263018b15dff36210e43546bc1bf
resellernewby <33678345+resellernewby@users.noreply.github.com>
2024-04-04 08:49:50 +0700

    add sendFileMessage

--- wweb.js
+++ wweb.js
@@ -24,3 +24,35 @@ window.WWebJS.sendMessageToId = async (chatId, message) => {
     return false;
   }
 };
+
+window.WWebJS.sendMediaMessage = async (
+  base64,
+  chatId,
+  filename,
+  caption,
+  option = {}
+) => {
+  try {
+    const numberExist = await window.WWebJS.checkNumberStatus(chatId); // xxxxxxx@c.us
+
+    if (!numberExist) return false;
+      
+    // prepare
+    const arr = base64.split(",");
+    let mime = arr[0].match(/(?:data:)?(.*?)(?:;base64)?$/i)[1];
+    mime = mime.split(/\s+;\s+/).join("; "); // Fix spaces, like "audio/ogg; codecs=opus"
+    type = mime.match(/^(.*?)\//)[1];
+    
+    const msg = await WPP.chat.sendFileMessage(chatId, base64, {
+        type: type,
+        caption: caption, // Optional
+        filename: filename, // Optional
+        mimetype: mime // Optional
+    });
+      
+    return true;
+    // return { msg: msg, status: true };
+  } catch (error) {
+    return false;
+  }
+};
